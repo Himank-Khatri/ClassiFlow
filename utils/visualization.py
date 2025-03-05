03b01570c6b739370fe96dc21508f34d24799605
Himank <86199877+Himank-Khatri@users.noreply.github.com>
2025-03-05 10:02:04 +0530

    Update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -20,7 +20,7 @@ def display_numeric_distribution(dataset):
     numeric_cols = dataset.select_dtypes(include=['int64', 'float64']).columns
     numeric_df = dataset[numeric_cols]
     fig = px.box(numeric_df, title='Numeric Data Distribution')
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, key=f'{fig}')
     
 def display_categorical_distribution(dataset):
     categorical_cols = dataset.select_dtypes(include=object).columns
@@ -36,12 +36,12 @@ def display_correlation_heatmap(dataset):
     fig = px.imshow(correlation_matrix, text_auto=True, title="Correlation Heatmap", height=700)
     fig.update_yaxes(showticklabels=True, tickangle=-45)
     fig.update_xaxes(showticklabels=True, tickangle=315)
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, key=f'{fig}')
 
 def display_scatter_matrix(dataset):
     numeric_cols = dataset.select_dtypes(include=['int64', 'float64']).columns
     numeric_df = dataset[numeric_cols]
     fig = px.scatter_matrix(numeric_df, title="Scatter Matrix")
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, key=f'{fig}')
 
     
